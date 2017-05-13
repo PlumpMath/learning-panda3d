80309fe47e21823219a50c4702b079935278f4f9
Tim Nibert <docz2a@gmail.com>
2017-05-13 19:27:01 +1000

    added a task to spin the camera

--- makeascene.py
+++ makeascene.py
@@ -1,10 +1,13 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
- 
+from direct.task import Task
+
 class MyApp(ShowBase):
- 
+
     def __init__(self):
         ShowBase.__init__(self)
- 
+
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
         # Reparent the model to render.
@@ -12,7 +15,18 @@ class MyApp(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
- 
- 
+
+        #add spin camera task to task manager
+        self.taskMgr.add(self.spinCamera, "SpinCameraTask")
+
+
+    #this is how we spin the camera
+    def spinCamera(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
 app = MyApp()
 app.run()
